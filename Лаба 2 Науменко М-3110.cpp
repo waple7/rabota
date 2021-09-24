e41ba8af24760a85b095fb73d447c3fa6cf3f4c5
waple7 <91288621+waple7@users.noreply.github.com>
2021-09-24 03:07:18 +0300

    Add files via upload

---
+++ Лаба 2 Науменко М-3110.cpp
@@ -0,0 +1,29 @@
+﻿#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    float z1, z2, b1, b2;
+    scanf_s("%f", &b1);
+    scanf_s("%f", &b2);
+    if ((b1 * b1 - 4) > 0)
+    {
+        z1 = (sqrt(2 * b1 + 2 * sqrt(b1 * b1 - 4))) / (sqrt(b1 * b1 - 4) + b1 + 2);
+        printf("%f", z1);
+    }
+    else
+    {
+        printf("net kornei");
+    }
+    printf("    ");
+    if ((b2 + 2) > 0)
+    {
+        z2 = 1 / (sqrt(b2 + 2));
+        printf("%f", z2);
+    }
+    else
+    {
+        printf("net kornei");
+    }
+    return 0;
+}
