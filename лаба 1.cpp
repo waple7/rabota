f5ae62a1ddb0cc2a9b2ac69529dd278ec7292fcb
waple7 <91288621+waple7@users.noreply.github.com>
2021-09-24 03:06:24 +0300

    Add files via upload

---
+++ лаба 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    char simvol;
+    scanf_s("%c", &simvol);
+    printf("%c\n", simvol);
+    {
+        float var;
+        scanf_s("%f", &var);
+        printf("%f\n", var);
+    }
+    return 0;
+}
+	
+
